288d6cdc55a229c7be8a147b49400cd2c6767e74
jsyeh <jsyeh.org@gmail.com>
2025-11-20 17:03:52 +0800

    week11

--- week11/week11-1b.cpp
+++ week11/week11-1b.cpp
@@ -1,1 +1,16 @@
+// week11-1a.cpp
+// SOIT106_ADVANCE_004
+#include <iostream>
+#include <cctype>
+using namespace std;
 
+int main()
+{
+	char c;
+	while ( cin >> c ) {
+		if ( c>='A' && c<='Z') c = c - 'A' + 'a';
+		else if(c>='a' && c<='z') c = c - 'a' + 'A';
+		cout << c;
+	}
+	cout << "\n";
+}
